e350ec0a0c3747a151508bde36612614e8d33344
Almaz Khalikov <47355790+almazkhalikov@users.noreply.github.com>
2023-01-13 10:00:01 +0300

    Create dirlist_creator.py
    https://gist.github.com/prabindh/35dfb54249a0b5cfc1550aa8c4bd72f1

---
+++ dirlist_creator.py
@@ -0,0 +1,18 @@
+#!python3
+
+# https://gist.github.com/prabindh/35dfb54249a0b5cfc1550aa8c4bd72f1
+
+import os
+import json
+
+def path_to_dict(path):
+    d = {os.path.basename(path): {}}
+    if os.path.isdir(path):
+        d[os.path.basename(path)]['type'] = "directory"
+        d[os.path.basename(path)]['children'] = dict((key,d[key]) for d in [path_to_dict(os.path.join(path,x)) for x in os.listdir(path)] for key in d)
+    else:
+        d[os.path.basename(path)]['type'] = "file"
+    return d
+
+with open('MANIFEST.JSON', 'w', encoding='utf-8') as f:
+    json.dump(path_to_dict('.'), f, ensure_ascii=False, indent=4)
